94dba080ce413ef1b52d7de7fddaf5ce5d2ce0ed
ptrierweiler <ptrierweiler@gmail.com>
2015-07-24 16:57:01 -0300

    fixed the comma issue

--- process_lotofacil.py
+++ process_lotofacil.py
@@ -12,22 +12,27 @@ csv_file = open('/home/ptrierweiler/Documents/lotto/lotofacil.csv','w')
 #Chaning Direction
 os.chdir('/home/ptrierweiler/Documents/lotto')
 
-#checking for zip file and deleting if exists
-if os.path.isfile(lotto_zip) == True:
-    print "Deleting previous vertion of " + lotto_zip + "\n"
-    os.remove(lotto_zip)
+def dwn_unzip_func():
+    #checking for zip file and deleting if exists
+    if os.path.isfile(lotto_zip) == True:
+        print "Deleting previous vertion of " + lotto_zip + "\n"
+        os.remove(lotto_zip)
 
-#checking for data directory and deleting if exists
-if os.path.isdir("data") == True:
-    shutil.rmtree("data")
+    #checking for data directory and deleting if exists
+    if os.path.isdir("data") == True:
+        shutil.rmtree("data")
 
-#downloading file using wget
-#should figure out how via python
-os.system("wget {url}".format(url = url))
+    #downloading file using wget
+    #should figure out how via python
+    os.system("wget {url}".format(url = url))
 
-#unziping data to data directory
-#should figure out using python
-os.system("unzip {zip} -d data".format(zip = lotto_zip))
+    #unziping data to data directory
+    #should figure out using python
+    os.system("unzip {zip} -d data".format(zip = lotto_zip))
+
+
+#calling download and unzip_func
+dwn_unzip_func()
 
 #Parse the html file
 html = open(html_file,'r')
@@ -75,24 +80,24 @@ def num_func(i):
         return i
 
 writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-writer.writerows(['game','date','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15'])
+writer.writerow(['game','date','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15'])
 for i in master_list:
     num = num_func(i[0])
     date = date_func(i[1])
-    n1 = i[2]
-    n2 = i[3]
-    n3 = i[4]
-    n4 = i[5]
-    n5 = i[6]
-    n6 = i[7]
-    n7 = i[8]
-    n8 = i[9]
-    n9 = i[10]
-    n10 = i[11]
-    n11 = i[12]
-    n12 = i[13]
-    n13 = i[14]
-    n14 = i[15]
-    n15 = i[16]
-    #print num,date,n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,n14,n15
-    writer.writerows([ num,date,n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,n14,n15])
+    n1 = i[2].strip('\n')
+    n2 = i[3].strip('\n')
+    n3 = i[4].strip('\n')
+    n4 = i[5].strip('\n')
+    n5 = i[6].strip('\n')
+    n6 = i[7].strip('\n')
+    n7 = i[8].strip('\n')
+    n8 = i[9].strip('\n')
+    n9 = i[10].strip('\n')
+    n10 = i[11].strip('\n')
+    n11 = i[12].strip('\n')
+    n12 = i[13].strip('\n')
+    n13 = i[14].strip('\n')
+    n14 = i[15].strip('\n')
+    n15 = i[16].strip('\n')
+    print num,date,n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,n14,n15
+    #writer.writerow([num,date,n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,n14,n15])
